a1c97b6e3f94c835851a16c398e3ad34fabcb389
Saran <potatohappy230@gmail.com>
2025-05-14 15:37:20 +0530

    added hybrid class

--- main.py
+++ main.py
@@ -2,6 +2,27 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 import joblib
+from sklearn.base import BaseEstimator, ClassifierMixin
+from scipy.stats import mode
+
+class HybridModel(BaseEstimator, ClassifierMixin):
+    def __init__(self, model1, model2, model3):
+        self.model1 = model1
+        self.model2 = model2
+        self.model3 = model3
+
+    def fit(self, X, y):
+        self.model1.fit(X, y)
+        self.model2.fit(X, y)
+        self.model3.fit(X, y)
+        return self
+
+    def predict(self, X):
+        pred1 = self.model1.predict(X)
+        pred2 = self.model2.predict(X)
+        pred3 = self.model3.predict(X)
+        final_pred = mode([pred1, pred2, pred3], axis=0)[0].flatten()
+        return final_pred
 
 app = FastAPI()
 
